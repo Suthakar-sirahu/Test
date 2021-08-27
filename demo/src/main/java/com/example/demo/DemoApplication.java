23469001f25bec9374eb4721d1b83d6cc268cebd
Suthakar-sirahu <smayandi@sirahu.com>
2021-08-27 18:59:48 +0530

    test1

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("welcome to modify... updates1111");
+		System.out.println("welcome to modify... updates1111d");
 
 		SpringApplication.run(DemoApplication.class, args);
 	}
